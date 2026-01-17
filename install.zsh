5275932ecd331a679ae6d67ba7e795bc4a1a6441
Jack Thorp <jackthorp13@gmail.com>
2026-01-17 23:51:07 +0000

    Add --adopt flag to install script.
    Prior to this change, stow would fail is the file/folder already existed
    in the target directory and is different to the stowed version.
    
    This change adds the --adopt flag which updates the stowed version to
    match the one in the target folder. Since the stowed dotfile packages
    are under version control it is then possible to diff the changes and
    make a decision about what to do.

--- install.zsh
+++ install.zsh
@@ -4,13 +4,18 @@
 DOTFILES_DIR=${0:a:h}
 TARGET_DIR='~'
 
+# TODO: add a dry-run option which uses the '--simulate' flag. 
+# TODO: consider using the '--adopt' flag to bring changes from conflicts in target directory into stow directories so they can be diffed via version control.
+# TODO: add a removal option that uses the '--delete' flag?
+#
+
 # Stow every directory in this directory apart from 
 # The benefit of stow is that for dotfiles such as .config (that lots of apps use) we don't 
 # simply overwrite the whole directory. Instead stow only symlinks the missing children in order
 # to make the stow and target directories match.
 for f in `ls -d */`; do
     echo "stowing $f"
-    stow -v "$f"
+    stow --verbose=0 --adopt "$f"
 done
 
 echo "All done!"
