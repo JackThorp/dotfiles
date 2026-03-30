be739acba3f3c9d25c090b9ed23bf3091c612085
Jack Thorp <jackthorp13@gmail.com>
2026-03-30 10:35:20 +0100

    update treesitter initialisation

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,16 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	run = ":TSUpdate",
+	build = ":TSUpdate",
 	-- This isn't the way I've seen plugins set up before... usually just opts= {} but for some reason this needed doing here...
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			highlight = {
-				enable = true,
-			},
+	opts = {
+		highlight = {
+			enable = true,
 			additional_vim_regex_highlighting = true,
-			ensure_installed = { "python", "c", "lua", "vim", "markdown", "vimdoc" },
-			indent = {
-				enable = true,
-				disable = { "yaml" },
-			},
-		})
-	end,
+		},
+		ensure_installed = { "python", "c", "lua", "vim", "markdown", "vimdoc" },
+		indent = {
+			enable = true,
+			disable = { "yaml" },
+		},
+	},
 }
